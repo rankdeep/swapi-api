a66ea08d7c92eb3b6213145a829b72aa303e4507
rankdeep <rankdeep@gmail.com>
2020-01-23 12:38:21 +0530

    film urls updated

--- Film.php
+++ Film.php
@@ -49,6 +49,36 @@ class Film{
             $species_planet[] = 'http://localhost/swapi/planets/'.$planet['id'];
         }
 
+        $query="SELECT * FROM species WHERE id IN (SELECT species_id FROM species_planet WHERE film_id = ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->episode_id);
+        $stmt->execute();
+        $species = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $species_planet = [];
+        foreach ($species as $key => $species) {
+            $species_planet[] = 'http://localhost/swapi/species/'.$species['id'];
+        }
+      
+        $query="SELECT * FROM vehicles WHERE id IN (SELECT vehicle_id FROM vehicle_films WHERE film_id = ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->episode_id);
+        $stmt->execute();
+        $vehicles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $vehicle_films = [];
+        foreach ($vehicles as $key => $vehicle) {
+            $vehicle_films[] = 'http://localhost/swapi/vehicles/'.$vehicle['id'];
+        }
+
+        $query="SELECT * FROM starships WHERE id IN (SELECT starship_id FROM starship_pilots WHERE film_id = ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->episode_id);
+        $stmt->execute();
+        $starships = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $starship_pilots = [];
+        foreach ($starships as $key => $starship) {
+            $starship_pilots[] = 'http://localhost/swapi/starships/'.$starship['id'];
+        }
+
 
         $this->title = $film['title'];
         $this->episode_id = $film['episode_id'];
@@ -58,9 +88,9 @@ class Film{
         $this->release_date = $film['release_date'];
         $this->characters = $film_characters;
         $this->planets = $species_planet;
-        $this->starships = [];
-        $this->vehicles = [];
-        $this->species = [];
+        $this->starships = $starship_pilots;
+        $this->vehicles = $vehicle_films;
+        $this->species = $species_planet;
         $this->url = "http://localhost/swapi/films/{$film['episode_id']}";
 }
 }
